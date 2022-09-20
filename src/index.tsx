daa1af02cafa8e80c2ec00c2cd698d0c51d53ea6
Lucas <lucas.bergamini05@outlook.com>
2022-09-20 14:56:37 -0300

    Criando primeira página

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
+import Cardapio from './pages/cardápio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div>Olá mundo!</div>
+    <Cardapio/>
   </React.StrictMode>
 );
-
