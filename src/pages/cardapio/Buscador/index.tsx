8fa03fed1732790065f8ce7caa91ab9cf776fe8d
Lucas <lucas.bergamini05@outlook.com>
2022-09-22 15:24:12 -0300

    Iniciando Cardapio e atualizando estilo do Header

---
+++ src/pages/cardapio/Buscador/index.tsx
@@ -0,0 +1,17 @@
+import styles from './Buscador.module.scss'
+
+interface IProps{
+  busca: string,
+  setBusca: React.Dispatch<React.SetStateAction<string>>,
+}
+
+export default function Buscador({busca, setBusca}: IProps){
+  return(
+    <div className={styles.buscador}>
+      <input
+        value={busca}
+        onChange={evento => setBusca(evento.target.value)}
+      />
+    </div>
+  )
+}
