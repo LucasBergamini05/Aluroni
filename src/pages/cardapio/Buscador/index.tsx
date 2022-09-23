d1c4d90b4f10bae2afb5b7b2938cc4afd82f6dc4
Lucas <lucas.bergamini05@outlook.com>
2022-09-23 15:24:48 -0300

    Definindo placeholder de busca

--- src/pages/cardapio/Buscador/index.tsx
+++ src/pages/cardapio/Buscador/index.tsx
@@ -11,6 +11,7 @@ export default function Buscador({busca, setBusca}: IProps){
       <input
         value={busca}
         onChange={evento => setBusca(evento.target.value)}
+        placeholder='Buscar'
       />
     </div>
   )
