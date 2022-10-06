811e76c11c67f87a61cfa760724b173d61274756
Lucas <lucas.bergamini05@outlook.com>
2022-10-05 23:00:01 -0300

    Criando página Sobre

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Main from 'components/Main';
 import Menu from 'components/Menu';
 import Cardapio from 'pages/cardapio';
 import Inicio from 'pages/inicio';
+import Sobre from 'pages/sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
@@ -12,6 +13,7 @@ export default function AppRouter() {
         <Route path='/' element={<Main/>}>
           <Route index element={<Inicio/>} />
           <Route path='cardapio' element={<Cardapio/>}/>
+          <Route path='sobre' element={<Sobre/>}/>
         </Route>
       </Routes>
     </Router>
